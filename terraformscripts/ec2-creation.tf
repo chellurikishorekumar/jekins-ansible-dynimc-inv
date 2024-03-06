e77a8878c205f1aaf0bec8ffa0609d5deb5beeb3
Chelluri kishore kumar <71449481+chellurikishorekumar@users.noreply.github.com>
2024-03-06 18:08:06 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,13 +2,12 @@ provider "aws"{
 region = "ap-southeast-2"
 }
 
-
 resource "aws_instance"  "AWSServer"{
 ami = "ami-023eb5c021738c6d0"
 instance_type = "t2.micro"
 key_name = "devopsdecbatch"
 security_groups = ["launch-wizard-2"]
-tags = {
+tags={
 Name = "tomcatservers"
 }
 }
