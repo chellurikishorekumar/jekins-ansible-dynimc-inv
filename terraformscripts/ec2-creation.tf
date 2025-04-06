aa54a62a235a16490befcc3be58e1f5e3b3646f9
Chelluri kishore kumar <71449481+chellurikishorekumar@users.noreply.github.com>
2025-04-06 21:03:37 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "AWSEC2Instance"{
    ami = "ami-0a9a48ce4458e384e"
    instance_type = "t2.micro"
-   key_name = "ansible.pem"
+   key_name = "ansible"
    vpc_security_group_ids = ["sg-08f9c03bc09f025d9"]
    tags={
    Name = "terraformserver"
